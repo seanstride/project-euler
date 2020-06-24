52f035c0f7cbd68bcd85c1ffcfe7236fb85b0ada
Sean Stride <seanstride@gmail.com>
2020-06-24 14:00:38 +0100

    Implementing problem 2

--- problem-2/Solution.cs
+++ problem-2/Solution.cs
@@ -1,6 +1,25 @@
+using System;
+using System.Collections.Generic;
+
 namespace problem_2
 {
-    public class Solution
+    public class FibonacciGenerator
     {
+        public List<int> GenerateSequence(int limit)
+        {
+            var sequence = new List<int>() { 1, 2 };
+
+            var nextNumberInSequence = 0;
+            while (nextNumberInSequence < limit)
+            {
+                nextNumberInSequence = sequence[sequence.Count - 2] + sequence[sequence.Count - 1];
+                if (nextNumberInSequence < limit)
+                {
+                    sequence.Add(nextNumberInSequence);
+                }
+            }
+
+            return sequence;
+        }
     }
 }
--- problem-2/UnitTests.cs
+++ problem-2/UnitTests.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Linq;
 using Xunit;
 
 namespace problem_2
 {
-    public class UnitTest1
+    public class GivenTheTermsInTheFibonacciSequenceBelowNinety
     {
         [Fact]
-        public void Test1()
+        public void WhenTheEvenNumbersInTheSequenceAreSummedTogetherThenTheResultIsFourtyFour()
         {
-
+            var fibonacciGenerator = new FibonacciGenerator();
+            var sequence = fibonacciGenerator.GenerateSequence(90);
+            var result = sequence.Where(x => x % 2 == 0).Sum();
+            Assert.Equal(44, result);
+        }
+    }
+    public class GivenTheTermsInTheFibonacciSequenceBelowFourMillion
+    {
+        [Fact]
+        public void WhenTheEvenNumbersInTheSequenceAreSummedTogetherThenTheResultIsX()
+        {
+            var fibonacciGenerator = new FibonacciGenerator();
+            var sequence = fibonacciGenerator.GenerateSequence(4000000);
+            var result = sequence.Where(x => x % 2 == 0).Sum();
+            Assert.Equal(4613732, result);
         }
     }
 }
